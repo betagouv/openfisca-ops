799ccba2a40a13996a9124339896618bbae7dadd
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-30 21:35:33 +0200

    Decrease false positives in API changes

--- utils/list_api_changes.sh
+++ utils/list_api_changes.sh
@@ -7,6 +7,6 @@ git fetch --all
 from=${1:-HEAD}
 to=${2:-openfisca/master}
 
-git log $from..$to | egrep '(Rename|Introduce|Deprecate)' | egrep -v '(\(again\)|fixup!)' | sort | uniq
+git log $from..$to | egrep '(Rename|Introduce|Deprecate) ' | egrep -v '(\(again\)|fixup!)' | sort | uniq
 
 echo "(Listed API changes between $from (`git rev-parse --short $from`) and $to (`git rev-parse --short $to`))"
