d1393e3ee0436423f5957e99dd70bf1af1c43319
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-30 21:26:48 +0200

    Add Python installation in update script

--- update.sh
+++ update.sh
@@ -6,3 +6,6 @@ git checkout master
 git pull origin master
 git submodule sync
 git submodule update --init --recursive
+
+git submodule foreach python setup.py develop
+git submodule foreach pip install --editable .
