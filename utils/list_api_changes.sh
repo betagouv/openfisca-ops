5851c0f1f6b364601485ecd816fd811655bd3e55
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-30 21:09:45 +0200

    Add OpenFisca-France API changes list script

---
+++ utils/list_api_changes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd openfisca/france
+
+git fetch --all
+
+from=${1:-sgmap/master}
+to=${2:-openfisca/master}
+
+git log $from..$to | egrep '(Rename|Introduce|Deprecate)' | egrep -v '(\(again\)|fixup!)' | sort | uniq
+
+echo "(Listed API changes between $from and $to)"
