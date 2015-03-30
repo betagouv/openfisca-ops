d506362f0a8704e034354b2a26ccd1155fa9c4ae
Matti Schneider <matti.schneider@data.gouv.fr>
2015-03-30 21:35:17 +0200

    Disambiguate compared revisions in API changes list

--- utils/list_api_changes.sh
+++ utils/list_api_changes.sh
@@ -9,4 +9,4 @@ to=${2:-openfisca/master}
 
 git log $from..$to | egrep '(Rename|Introduce|Deprecate)' | egrep -v '(\(again\)|fixup!)' | sort | uniq
 
-echo "(Listed API changes between $from and $to)"
+echo "(Listed API changes between $from (`git rev-parse --short $from`) and $to (`git rev-parse --short $to`))"
