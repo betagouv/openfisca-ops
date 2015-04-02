222fd2fb247e77843a3412a49d69aa425874dfef
Matti Schneider <matti.schneider@data.gouv.fr>
2015-04-02 17:25:29 +0200

    Minor refactor of formula integration test

--- test/integration/formula.sh
+++ test/integration/formula.sh
@@ -5,7 +5,9 @@ cd `dirname $0`
 HOST=${1:-localhost:2000}
 FORMULAS_LIST_PATH=${2:-formula_taxes.txt}
 
-echo "Testing on $HOST (specify target host as first parameter)"
+target="http://$HOST/api/2/formula/"
+
+echo "Testing on $target (specify host as first parameter)"
 echo "---------------------------------------------------------"
 
 failures=0
@@ -14,7 +16,7 @@ for tax in `cat $FORMULAS_LIST_PATH`
 do
 	echo -n $tax,
 
-	if ! lwp-request -sd -m GET http://$HOST/api/2/formula/$tax
+	if ! lwp-request -sd -m GET $target/$tax
 	then let failures++
 	fi
 done
