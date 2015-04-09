a8a51dbc951cc643e0e8700830ab968e0c3ffe78
fpagnoux <fpagnoux@octo.com>
2015-04-09 18:20:03 +0200

    Fix ubuntu bootstrap script
    - Install the packages without user interaction (--assume-yes)
    - Don't install pip with apt get, but with easy_install in the common bootrstrap script
    - Update apt-get before installing anything

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -1,9 +1,11 @@
 # Prerequisites
-apt-get install gettext
-apt-get install git
-apt-get install python-pip
-apt-get install python-dev
-apt-get install python-numpy
-apt-get install python-pymongo
-apt-get install python-scipy
-apt-get install gfortran
+
+apt-get update
+apt-get --assume-yes install gettext
+apt-get --assume-yes install git
+apt-get --assume-yes install python-setuptools
+apt-get --assume-yes install python-dev
+apt-get --assume-yes install python-numpy
+apt-get --assume-yes install python-pymongo
+apt-get --assume-yes install python-scipy
+apt-get --assume-yes install gfortran
