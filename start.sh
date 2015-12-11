997dba47e05e0dd99a83d4f3488bc5fca7f3a7bc
Matti Schneider <matti.schneider@data.gouv.fr>
2015-12-11 18:26:45 +0100

    Allow setting port at runtime

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # 1st param = config name. Defaults to `development`.
 # Available configs are in `config` folder.
+PORT=${PORT:-2000}
 
-paster serve --reload `dirname $0`/config/${1:-development}.ini
+cd `dirname $0`
+
+sed s/%PORT%/$PORT/ config/${1:-development}.ini > config/current.ini
+
+paster serve --reload config/current.ini
