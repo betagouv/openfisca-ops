c07ab7db2bd82fc382d6cc994b94791b60f7db77
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-02-25 18:50:55 +0100

    Refactor using git submodule foreach

--- update.sh
+++ update.sh
@@ -5,12 +5,6 @@ set -ex
 
 cd `dirname $0`
 
-# Sandbox the install to the virtualenv, or, if not available, to the user.
-if [[ ! $VIRTUAL_ENV ]]
-then
-	user_option='--user'
-fi
-
 if [[ -n $1 ]]
 then
 	git fetch origin
@@ -20,23 +14,10 @@ fi
 git submodule sync
 git submodule update --init --recursive
 
-for submodule in core web-api france parsers
-do
-	cd openfisca/$submodule/
-	python setup.py develop $user_option
-	pip install $user_option --editable .
-	cd -
-done
 
 for old_symlink_extension in $(find openfisca/france/openfisca_france/model/extensions -type l)
 do
 	rm $old_symlink_extension
 done
 
-for extension in $(ls -d openfisca/extensions/*)
-do
-	if git submodule | grep $extension
-	then
-		ln -s $(pwd)/$extension openfisca/france/openfisca_france/model/extensions/
-	fi
-done
+git submodule foreach '$toplevel/install_submodule.sh $path $toplevel'
