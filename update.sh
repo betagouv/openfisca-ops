5cb54d13167c620a2d412b7e25b3f54c9d7db281
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-04-15 11:16:29 +0200

    TMP: Fixup for use on windows
    DO NOT MERGE: this is too violent, will remove everything in extensions
    Just to be able to work with Paris

--- update.sh
+++ update.sh
@@ -15,9 +15,9 @@ git submodule sync
 git submodule update --init --recursive
 
 
-for old_symlink_extension in $(find openfisca/france/openfisca_france/model/extensions -type l)
+for old_symlink_extension in $(find openfisca/france/openfisca_france/model/extensions/* -type dl)
 do
-	rm $old_symlink_extension
+	rm -rf $old_symlink_extension
 done
 
 git submodule foreach '$toplevel/install_submodule.sh $path $toplevel'
