dc5e5f7c57be8bf71683ef7df9a2e8fc2f9af2ee
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-07-02 00:47:16 +0200

    Use start.sh without param to start mes-aides

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 1st param = config name. Defaults to `development`.
-# Available configs are in `config` folder.
 
 cd `dirname $0`
 
-paster serve --reload config/${1:-development}.ini
+./generateMesAidesConfig.sh
+
+paster serve --reload config/current.ini
